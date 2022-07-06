73e82c7a46449581dd3b45e15d7c7179a2e9a656
1lubo <1lubo@pm.me>
2022-07-06 17:38:32 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# airports = Airport.create([{ name: 'Chattanooga', code: 'CHA' }, { name: 'Knoxville', code: 'TYS' },
-#                           { name: 'Memphis', code: 'MEM' }, { name: 'Nashville', code: 'BNA' }, { name: 'Tri-Cities', code: 'TRI' },
-#                           { name: 'Jackson', code: 'MKL' }, { name: 'Clarksville', code: 'CKV' }, { name: 'Smyrna', code: 'MQY' },
-#                           { name: 'Athens', code: 'MMI' }, { name: 'Centerville', code: 'GHM' }, { name: 'Cleveland', code: 'KRZR' },
-#                           { name: 'Columbia / Mt. Pleasant', code: 'MRC' }, { name: 'Crossville', code: 'CSV' },
-#                           { name: 'Dyersburg ', code: 'KDYR' }, { name: 'Fayetteville', code: 'FYM' }, { name: 'Gallatin', code: 'XNX' },
-#                           { name: 'Greeneville', code: 'CGY' }, { name: 'Huntingdon / McKenzie', code: 'KHZD' },
-#                           { name: 'Jacksboro', code: 'KJAU' }, { name: 'Jasper', code: 'APT' }, { name: 'Lewisburg', code: 'KLUG' },
-#                           { name: 'Lexington / Parsons', code: 'KPVE' }])
-#
+airports = Airport.create([{ name: 'Chattanooga', code: 'CHA' }, { name: 'Knoxville', code: 'TYS' },
+                           { name: 'Memphis', code: 'MEM' }, { name: 'Nashville', code: 'BNA' }, { name: 'Tri-Cities', code: 'TRI' },
+                           { name: 'Jackson', code: 'MKL' }, { name: 'Clarksville', code: 'CKV' }, { name: 'Smyrna', code: 'MQY' },
+                           { name: 'Athens', code: 'MMI' }, { name: 'Centerville', code: 'GHM' }, { name: 'Cleveland', code: 'KRZR' },
+                           { name: 'Columbia / Mt. Pleasant', code: 'MRC' }, { name: 'Crossville', code: 'CSV' },
+                           { name: 'Dyersburg ', code: 'KDYR' }, { name: 'Fayetteville', code: 'FYM' }, { name: 'Gallatin', code: 'XNX' },
+                           { name: 'Greeneville', code: 'CGY' }, { name: 'Huntingdon / McKenzie', code: 'KHZD' },
+                           { name: 'Jacksboro', code: 'KJAU' }, { name: 'Jasper', code: 'APT' }, { name: 'Lewisburg', code: 'KLUG' },
+                           { name: 'Lexington / Parsons', code: 'KPVE' }])
+
 airports = Airport.all
 duration = %w[30 60 90 120 150 180]
 hours = (6..18).to_a
